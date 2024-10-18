bf950011150a1d9c57772db45dd29ffd111fc8bc
sebmuel <mueller.sebastian117@protonmail.com>
2024-10-18 19:58:33 +0200

    Init

---
+++ CleanArchitecture.Umbraco.ClassLibrary/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Umbraco.ClassLibrary;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddLayer(this IServiceCollection services)
+    {
+        return services;
+    }
+}
